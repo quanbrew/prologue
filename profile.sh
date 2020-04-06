0f0cdfe4968024aba920ff9dd85d78d05ca0eb09
coppa <sortal@protonmail.com>
2020-04-07 05:35:11 +0800

    emacs bin

--- profile.sh
+++ profile.sh
@@ -5,10 +5,16 @@ export LC_CTYPE=en_US.UTF-8
 export EDITOR=nvim
 export VISUAL=nvim
 
-CARGO_HOME="$HOME/.cargo"
-if [[ -d "$CARGO_HOME" ]]; then
-    export PATH="$CARGO_HOME/bin:$PATH"
+CARGO_BIN="$HOME/.cargo/bin"
+if [[ -d "$CARGO_BIN" ]]; then
+    export PATH="$CARGO_BIN:$PATH"
 fi
+
+EMACS_BIN="$HOME/.emacs.d/bin"
+if [[ -d "$EMACS_BIN" ]]; then
+    export PATH="$EMACS_BIN:$PATH"
+fi
+
 if [[ -x "$(command -v yarn)" ]]; then
     export PATH="$(yarn bin):$PATH"
 fi
