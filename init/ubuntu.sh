3ff4b8ed0773057b14c53741600524527695bf95
coppa <t@ioover.net>
2020-05-30 17:22:43 +0800

    remove Python packages

--- init/ubuntu.sh
+++ init/ubuntu.sh
@@ -5,7 +5,7 @@ locale-gen zh_CN.UTF-8
 locale-gen en_US.UTF-8
 timedatectl set-timezone Asia/Shanghai
 apt-get update
-apt-get -y install build-essential zsh mosh python python-pip python3 python3-pip python-dev htop ntp vim neovim git tmux fish tldr
+apt-get -y install build-essential zsh mosh htop ntp vim neovim git tmux fish tldr
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
